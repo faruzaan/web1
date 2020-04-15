581e3dddc495becf736511b38d74edfe603491f7
unknown <farhan.ff.fauzaan@gmail.com>
2020-04-15 19:23:19 +0700

    perubahan

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login</title>
+	<title>Login V1</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="{{asset('assets')}}/img/title.jpg"/>
+	<link rel="icon" type="image/png" href="{{asset('login')}}/images/icons/favicon.ico"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('login')}}/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
@@ -15,11 +15,7 @@
 <!--===============================================================================================-->	
 	<link rel="stylesheet" type="text/css" href="{{asset('login')}}/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{asset('login')}}/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('login')}}/vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{asset('login')}}/vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('login')}}/css/util.css">
 	<link rel="stylesheet" type="text/css" href="{{asset('login')}}/css/main.css">
@@ -30,44 +26,50 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form p-l-55 p-r-55 p-t-178">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="{{asset('login')}}/images/img-01.png" alt="IMG">
+				</div>
+
+				<form class="login100-form validate-form">
 					<span class="login100-form-title">
-						Sign In
+						Member Login
 					</span>
 
-					<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
-						<input class="input100" type="text" name="username" placeholder="Username">
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email" placeholder="Email">
 						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Please enter password">
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
 						<input class="input100" type="password" name="pass" placeholder="Password">
 						<span class="focus-input100"></span>
-					</div>
-
-					<div class="text-right p-t-13 p-b-23">
-						<span class="txt1">
-							Forgot
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
 						</span>
-
-						<a href="#" class="txt2">
-							Username / Password?
-						</a>
 					</div>
-
+					
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
-							Sign in
+							Login
 						</button>
 					</div>
 
-					<div class="flex-col-c p-t-170 p-b-40">
-						<span class="txt1 p-b-9">
-							Don’t have an account?
+					<div class="text-center p-t-12">
+						<span class="txt1">
+							Forgot
 						</span>
+						<a class="txt2" href="#">
+							Password?
+						</a>
+					</div>
 
-						<a href="#" class="txt3">
-							Sign up now
+					<div class="text-center p-t-136">
+						<a class="txt2" href="#">
+							Create your Account
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
 					</div>
 				</form>
@@ -76,20 +78,22 @@
 	</div>
 	
 	
-<!--===============================================================================================-->
+
+	
+<!--===============================================================================================-->	
 	<script src="{{asset('login')}}/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="{{asset('login')}}/vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
 	<script src="{{asset('login')}}/vendor/bootstrap/js/popper.js"></script>
 	<script src="{{asset('login')}}/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
 	<script src="{{asset('login')}}/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="{{asset('login')}}/vendor/daterangepicker/moment.min.js"></script>
-	<script src="{{asset('login')}}/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="{{asset('login')}}/vendor/countdowntime/countdowntime.js"></script>
+	<script src="{{asset('login')}}/vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
 <!--===============================================================================================-->
 	<script src="{{asset('login')}}/js/main.js"></script>
 
